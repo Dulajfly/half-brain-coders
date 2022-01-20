28a3652702cb84e8054f0a6d00e638c71e14f634
Bartosz Jasak <mrbjasak@gmail.com>
2022-01-20 21:52:39 +0100

    [FRONTEND] Im drunk af and i make leaflet work in exitpoint form

--- base/models.py
+++ base/models.py
@@ -18,8 +18,8 @@ class ExitPoint(models.Model):
     ]
     name = models.CharField(max_length=255, null=True, blank=True)
     # country = CountryField(blank=Tru
-    tracking_difficulty_level = models.CharField(verbose_name=_('Tracking skill level'), max_length=200, choices=DIFFICULT_CHOICES, blank=True, null=True)
-    wingsuit_difficulty_level =models.CharField(verbose_name=_('Wingsuit skill level'), max_length=200, choices=DIFFICULT_CHOICES, blank=True, null=True)
+    tracking_difficulty_level = models.CharField(verbose_name=_('Tracking skill level'), max_length=200, choices= DIFFICULT_CHOICES, blank=True, null=True)
+    wingsuit_difficulty_level = models.CharField(verbose_name=_('Wingsuit skill level'), max_length=200, choices= DIFFICULT_CHOICES, blank=True, null=True)
     rock_drop_second = models.PositiveSmallIntegerField(validators=[MaxValueValidator(120), MinValueValidator(1)], null=True, blank=True)
     rock_drop_altitude = models.PositiveSmallIntegerField(validators=[MaxValueValidator(8849), MinValueValidator(10)], null=True, blank=True)
     landing_altitude = models.PositiveSmallIntegerField(validators=[MaxValueValidator(8000), MinValueValidator(1)], null=True, blank=True)
