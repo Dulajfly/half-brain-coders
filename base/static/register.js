e72e415f52b840cf34e4f0c1ddb3d1e9bd68e308
Bartosz Jasak <mrbjasak@gmail.com>
2022-01-27 20:47:36 +0100

    [FRONTEND] Add css

---
+++ base/static/register.js
@@ -0,0 +1,12 @@
+const password = document.getElementById('id_password1');
+const login = document.getElementById('id_password');
+const password2 = document.getElementById('id_password2');
+
+
+const username = document.getElementById('id_username');
+
+
+console.log(username)
+
+password.classList.add('form-control')
+password2.classList.add('form-control')
